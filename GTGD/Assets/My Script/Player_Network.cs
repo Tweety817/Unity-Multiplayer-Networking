339ee2a86f6bfb99d5dc0018f0421f46ca29175b
Tweety817 <tweety30119@gmail.com>
2018-09-18 16:39:38 +0800

    first_commit

---
+++ GTGD/Assets/My Script/Player_Network.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Player_Network : NetworkBehaviour {
+
+    public GameObject firstPersonCharacters;
+    public GameObject[] charactersModel;
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<FirstPersonController>().enabled = true;
+        firstPersonCharacters.SetActive(true);
+
+        foreach(GameObject go in charactersModel)
+        {
+            go.SetActive(false);
+        }
+    }
+}
